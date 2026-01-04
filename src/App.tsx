4ca0f4eb1ca7807f87bc8baa3a1c17c5df37a2c1
Jesus24-Dev <siritjesus24@gmail.com>
2026-01-04 14:16:22 -0400

    feat: refactor App component to use react-router for navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,70 +1,14 @@
-'use client';
-import { useState, useEffect, useCallback } from 'react';
-import { AnimatePresence } from 'framer-motion';
-import Home from './components/pages/Home';
-import About from './components/pages/About'; 
-import Projects from './components/pages/Projects'; 
-import Skills from './components/pages/Skills'; 
-import Navbar from './components/Header/Navbar/Navbar';
-import Contact from './components/pages/Contact';
+import {Routes, Route} from 'react-router-dom';
+import HomePage from "./pages/HomePage";
+import WorkflowDetail from './components/Workflow/WorkflowDetail';
 
-export type SectionType = 'Home' | 'About' | 'Projects' | 'Skills' | 'Contact';
-
-export default function App() {
-  const [activeSection, setActiveSection] = useState<SectionType>('Home');
-  const [isScrolling, setIsScrolling] = useState(false);
-
-  const scrollToSection = useCallback((id: SectionType) => {
-    const element = document.getElementById(id);
-    if (element) {
-      setIsScrolling(true);
-      element.scrollIntoView({
-        behavior: 'smooth',
-        block: 'start'
-      });
-      setTimeout(() => {
-        setActiveSection(id);
-        setIsScrolling(false);
-      }, 1000);
-    }
-  }, []);
-
-  useEffect(() => {
-    if (isScrolling) return;
-
-    const handleScroll = () => {
-      const scrollPosition = window.scrollY + window.innerHeight / 3;
-      const sections: SectionType[] = ['Home', 'About', 'Projects', 'Skills'];
-      
-      for (const section of sections) {
-        const element = document.getElementById(section);
-        if (element) {
-          const { offsetTop, offsetHeight } = element;
-          if (scrollPosition >= offsetTop && scrollPosition < offsetTop + offsetHeight) {
-            setActiveSection(section);
-            break;
-          }
-        }
-      }
-    };
-
-    window.addEventListener('scroll', handleScroll);
-    return () => window.removeEventListener('scroll', handleScroll);
-  }, [isScrolling]);
-
+function App() {
   return (
-    <div className="relative">
-      <Navbar activeSection={activeSection} scrollToSection={scrollToSection} />
-      
-      <main className="snap-y snap-mandatory h-screen overflow-y-auto">
-        <AnimatePresence>
-          <Home id="Home" />
-          <About id="About" /> 
-          <Projects id="Projects" />    
-          <Skills id="Skills" /> 
-          <Contact id="Contact"/>
-        </AnimatePresence>
-      </main>
-    </div>
+    <Routes>
+      <Route path="/" element={<HomePage />}/>
+      <Route path="/workflows/:id" element={<WorkflowDetail />} />
+    </Routes>
   );
 }
+
+export default App;
